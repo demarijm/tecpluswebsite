781be053cb5582ca93b6a54e303529084efe9ddf
Demari Miller <demarijmiller@gmail.com>
2023-06-20 15:31:37 -0400

    New comp added from shortcut

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/static';
 
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), alpinejs()]
+  site: 'https://cassyork.com',
+  integrations: [mdx(), sitemap(), tailwind(), alpinejs()],
+  adapter: vercel({
+    analytics: true
+  })
 });
