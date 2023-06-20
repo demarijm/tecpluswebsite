2287ce01365255183fa391392049ecd56439eb38
Demari Miller <demarijmiller@gmail.com>
2023-06-20 15:38:07 -0400

    New comp added from shortcut

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'TecPlus | Saas & Ecommerce | Software Development';
+export const SITE_DESCRIPTION = 'TecPlus is a software development firm that specializes in building Saas and Ecommerce applications.';
